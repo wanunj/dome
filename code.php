0cb04a9de6817bb48f0f971242ad76693af20555
unknown <>
2017-04-12 20:30:42 +0800

    封装验证码函数

--- code.php
+++ code.php
@@ -8,62 +8,15 @@
 * Author: Lee
 * Date: 2017-4-9
 */
+// 定义常量，用来授权调用includes里面的文件
+define('IN_TG',true);
+//引入公共文件
+require dirname(__FILE__).'/includes/common.inc.php';
+
 session_start();
-//随机码个数
-$_rnd_color=4;
 
-// 创建随机码
-$_nmsg='';
-for($i=0;$i<4;$i++){
-    $_nmsg .=dechex(mt_rand(0,15));
-}
-// 保存在session
-$_SESSION['code']=$_nmsg;
+// 运行验证码
+// 长 宽 位数 边框
+_code();
 
-// 长和高
-$_width=75;
-$_height=25;
-
-// 创建一张图像
-$_img=imagecreatetruecolor($_width,$_height);
-
-//白色
-$_white=imagecolorallocate($_img,255,255,255);
-
-//填充
-imagefill($_img,0,0,$_white);
-
-$_flag=false;
-if($_flag){
-    //黑色,边框
-    $_black=imagecolorallocate($_img,0,0,0);
-    imagerectangle($_img,0,0,$_width-1,$_height-1,$_black);
-}
-
-
-//随机画出6个线条
-for ($i=0;$i<6;$i++){
-    $_rnd_color=imagecolorallocate($_img,mt_rand(200,255),mt_rand(200,255),mt_rand(200,255));
-    imageline($_img,mt_rand(0,$_width),mt_rand(0,$_height),mt_rand(0,$_width),mt_rand(0,$_height),$_rnd_color);
-}
-
-// 随机雪花
-for($i=0;$i<100;$i++){
-    $_rnd_color=imagecolorallocate($_img,mt_rand(200,255),mt_rand(200,255),mt_rand(200,255));
-    imagestring($_img,1,mt_rand(1,$_width),mt_rand(1,$_height),'*',$_rnd_color);
-}
-
-//输出验证码
-// for ($i=0;$i<strlen($_SESSION['code']);$i++) {
-// 	$_rnd_color = imagecolorallocate($_img,mt_rand(0,100),mt_rand(0,150),mt_rand(0,200));
-// 	imagestring($_img,5,$i*$_width/$_rnd_code+mt_rand(1,10),mt_rand(1,$_height/2),$_SESSION['code'][$i],$_rnd_color);
-// }
-
-//输出图像
-ob_clean();
-header('Content-Type:image/png');
-imagePng($_img);
-
-//销毁
-imagedestroy($_img);
 ?>
--- register.php
+++ register.php
@@ -40,17 +40,17 @@ require dirname(__FILE__).'/includes/common.inc.php';
     <form action="post.php" name="register" method="post">
         <dl>
             <dt>请认真填写以下内容</dt>
-            <dd>用&nbsp;&nbsp;户&nbsp;&nbsp;名：<input type="text" name="username" class="text">(*必填，至少两位)</dd>
-            <dd>密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码：<input type="password" name="password" class="text">(*必填，至少六位)</dd>
-            <dd>确认密码：<input type="password" name="notpassword" class="text">(*同上)</dd>
-            <dd>密码提示：<input type="text" name="passt" class="text">(*必填，至少两位)</dd>
-            <dd>密码回答：<input type="text" name="passd" class="text">(*必填，至少两位)</dd>
+            <dd>用&nbsp;&nbsp;户&nbsp;&nbsp;名：<input type="text" name="username" class="text"> (*必填，至少两位)</dd>
+            <dd>密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码：<input type="password" name="password" class="text"> (*必填，至少六位)</dd>
+            <dd>确认密码：<input type="password" name="notpassword" class="text"> (*同上)</dd>
+            <dd>密码提示：<input type="text" name="passt" class="text"> (*必填，至少两位)</dd>
+            <dd>密码回答：<input type="text" name="passd" class="text"> (*必填，至少两位)</dd>
             <dd>性&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;别：<input type="radio" name="sex" value="男" checked="checked">男<input type="radio" name="sex" value="女">女</dd>
             <dd class="face"><input type="hidden" name="face" value="face/m01.gif"><img src="face/m01.gif" alt="头像选择" id="faceimg"></dd>
             <dd>电子邮件：<input type="text" name="email" class="text"></dd>
             <dd>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Q &nbsp;&nbsp;Q：<input type="text" name="qq" class="text"></dd>
             <dd>主页地址：<input type="text" name="url" class="text" value="http://"></dd>
-            <dd>验&nbsp;&nbsp;证&nbsp;&nbsp;码：<input type="text" name="yzm" class="text yzm"><img src="code.php" /></dd>
+            <dd>验&nbsp;&nbsp;证&nbsp;&nbsp;码：<input type="text" name="yzm" class="text yzm"><img src="code.php" id="code" /></dd>
             <dd><input type="submit" class="submit" value="注册"></dd>
         </dl>
     </form>
